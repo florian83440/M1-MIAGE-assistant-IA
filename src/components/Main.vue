dd31cd23172533a547d02eac4d3b0e335218b5c4
flo83440 <aimeflorian1@gmail.com>
2024-04-05 09:55:56 +0200

    main

---
+++ src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="mainContent">
+    <div class="screenPrint" style="background-color: yellow">
+      hello
+    </div>
+    <div class="entryText">
+      <input type="text">
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.mainContent{
+  height: 100vh;
+
+}.screenPrint{
+  height: 95vh;
+}
+.entryText{
+  height: 5vh;
+  right: 5%;
+  background-color: red;
+}
+.entryText input{
+  margin-left: 10%;
+  width: 80%;
+}
+</style>
